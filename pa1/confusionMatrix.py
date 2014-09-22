53f95a24088e80fe93919a969f864b734dbf3cb9
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-09-21 22:33:29 -0400

    added a method which prints confision matrix

--- pa1/confusionMatrix.py
+++ pa1/confusionMatrix.py
@@ -1,16 +1,58 @@
-filename="PredictionOfzoo-test"
 ext=".csv"
-
 for i in range(1,3):
 	filename="results/PredictionOfzoo-test"
 	filename=filename+str(i)+ext
 	f = open(filename,"r")
 	lines=f.readlines()
 
-	distinctClasses=[]
-	totalCount=len(lines)
-	misClassificationCount=0
-	for l in lines:
-					
-	
 
+	distinctClasses={}
+	reverseIndex={}
+	totalCount=len(lines)
+	t=[]
+	liTuples=[]
+	count=0
+	for l in lines:
+		t=l.split()[0].split(',')
+		liTuples.append(t)
+		if t[1] not in distinctClasses:
+			distinctClasses[t[1]]=count
+			reverseIndex[count]=t[1]
+			count+=1
+	numClasses=len(distinctClasses.keys())
+
+
+	#create the matrix
+	cfm1=[0]
+	cfm2=cfm1*numClasses
+	cfm3=[cfm2]
+	cfm=cfm3*numClasses
+
+	cfm = [[0 for x in range(0,numClasses)] for x in range(0,numClasses)]
+
+	for t in liTuples:
+		#left entry in csv is predicted label
+		#right entry in csv is actual label
+		rowIndex=distinctClasses[t[1]]
+		columnIndex=distinctClasses[t[0]]		
+		cfm[rowIndex][columnIndex]=cfm[rowIndex][columnIndex]+1	
+
+	print "Confusion Matrix with depth - "+str(i)+":"	
+
+	headers = [reverseIndex[j] for j in range(0,numClasses)] 
+
+	print "	"+"	".join(headers)
+	count=0
+	for row in cfm:
+		pString=""	
+		for j in row:
+			pString+="	"+str(j)
+		print headers[count]+pString
+		count+=1
+	print
+	
+	
+	
+	
+	
+					
