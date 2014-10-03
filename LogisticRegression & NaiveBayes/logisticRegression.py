f25a226792550996992b84156c20901352481214
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-10-03 13:35:37 -0400

    fixed type conversion issues

--- LogisticRegression & NaiveBayes/logisticRegression.py
+++ LogisticRegression & NaiveBayes/logisticRegression.py
@@ -9,6 +9,7 @@ numFeatures=-1
 classIndex=-1
 learningRate=0.5
 
+
 #this method is used to read the csv file and save it in a list of lists.
 def readData(fileName):
 	data = []
@@ -28,13 +29,14 @@ def readData(fileName):
 #w is the weight vector
 #x is the feature vecotor
 #returns 1/(1+exp(w.x))
-def maxLikelyhood(w,x):
+def maxLikelyHood(w,x):
 	dotProduct=0
 	for i in range(0,numFeatures):
 		dotProduct+=(w[i]*x[i])
 	return float(1)/(1+math.exp(dotProduct))
 
 def main():
+
 	trainingSet=readData(trainingSetFileName)
 	if len(trainingSet) <=0:
 		print "No data in training set"
@@ -49,11 +51,12 @@ def main():
 	while True: #whats the condition to break???
 		gradientVector = [0 for i in range(0,numFeatures)]
 		for sample in trainingSet:
-			error = trainingSet[classIndex] - maxLikelyHood(weightVector,sample)
+			error = float(sample[classIndex]) - maxLikelyHood(weightVector,sample)
 			for j in range(0,numFeatures):
-				gradientVector[j]+=gradientVector[j]+error*sample[j]
+				gradientVector[j]+=gradientVector[j]+error*float(sample[j])
 				weightVector[j]+=learningRate*gradientVector[j]				
-				
+		print weightVector
+		break #for now...
 		
 #Execution begins here
 if __name__ == "__main__" : main()	
