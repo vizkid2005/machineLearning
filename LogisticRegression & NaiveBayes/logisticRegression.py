197b706ffe36e34adebd270e823849c2d01c6efd
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-10-03 17:40:11 -0400

    added the breaking condition

--- LogisticRegression & NaiveBayes/logisticRegression.py
+++ LogisticRegression & NaiveBayes/logisticRegression.py
@@ -7,8 +7,8 @@ trainingSetFileName="zoo-trainSimplified.csv"
 testSetFileName="zoo-testSimplified.csv"
 numFeatures=-1
 classIndex=-1
-learningRate=0.5
-
+learningRate=math.exp(5)
+threshold = math.exp(50)
 
 #this method is used to read the csv file and save it in a list of lists.
 def readData(fileName):
@@ -47,16 +47,33 @@ def main():
 
 	#initialize the weight vector to be 0's
 	weightVector = [0 for i in range(0,numFeatures)]	
-		
-	while True: #whats the condition to break???
+	weightVectorBackUp = [0 for i in range(0,numFeatures)]		
+	iterations=0
+
+	while True: 
+		iterations+=1
 		gradientVector = [0 for i in range(0,numFeatures)]
 		for sample in trainingSet:
 			error = float(sample[classIndex]) - maxLikelyHood(weightVector,sample)
 			for j in range(0,numFeatures):
 				gradientVector[j]+=gradientVector[j]+error*float(sample[j])
 				weightVector[j]+=learningRate*gradientVector[j]				
-		print weightVector
-		break #for now...
+						
+		#check whether the increment in weight vector is greater than the "threshold" for every feature...
 		
+		shouldBreak=True
+		for i in range(numFeatures):
+			if math.fabs(weightVector[i] - weightVectorBackUp[i]) > threshold:
+				shouldBreak=False		
+			
+		if shouldBreak==True:
+			break
+		else:
+			#take a backup of the weight vector...
+			weightVectorBackUp = [weightVector[i] for i in range(numFeatures)]
+
+	print weightVector
+	print "Number of Iterations "+str(iterations) 
+
 #Execution begins here
 if __name__ == "__main__" : main()	
