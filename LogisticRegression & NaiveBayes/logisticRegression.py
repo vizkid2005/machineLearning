42e890ac3dbcd5686d096a502fc2ba6b87353a82
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-10-09 20:51:44 -0400

    added the code which handles test data

--- LogisticRegression & NaiveBayes/logisticRegression.py
+++ LogisticRegression & NaiveBayes/logisticRegression.py
@@ -8,7 +8,7 @@ testSetFileName="zoo-testSimplified.csv"
 numFeatures=-1
 classIndex=-1
 learningRate=math.exp(5)
-threshold = math.exp(50)
+threshold = math.exp(100)
 
 #this method is used to read the csv file and save it in a list of lists.
 def readData(fileName):
@@ -25,6 +25,53 @@ def readData(fileName):
 		myFile.close()
 	return data	
 
+
+#w is the weight vector
+#row is a sample
+def predictLabel(row,w):
+	dotProduct=0
+	#computing dot product here
+	for i in range(numFeatures):
+		dotProduct+=(w[i]*row[i])
+	if dotProduct >= 0:
+		return "1"
+	return "0"
+
+#inputFileName - file name of the test set
+# w is the weight vector.
+#outputFileName - file name for the output.
+#consider 1 as + 
+# ans o as -ve
+def handleTestData(inputFileName,w,outputFileName):
+	tp=0
+	fp=0
+	tn=0
+	fn=0
+	samples=readData(inputFileName)
+	totalCount=len(samples)
+	if totalCount <=0:
+		print "Test data set is empty"
+		exit()
+	print "Printing (actual, predicted)"
+	for row in samples:
+		pLabel = predictLabel(row,w)
+		aLabel = row[classIndex] 
+		print aLabel,pLabel
+		if(aLabel=="1"):
+			if(pLabel=="1"):
+				tp+=1
+			else:
+				fn+=1			
+		else:
+			if (pLabel=="1"):
+				fp+=1
+			else:
+				tn+=1
+				
+	print "Total True positives is ",tp
+	print "Total True Negatives is ",tn
+	print "Total False Positives is ",fp
+	print "Total False Negatives is ",fn
 
 #w is the weight vector
 #x is the feature vecotor
@@ -74,6 +121,10 @@ def main():
 
 	print weightVector
 	print "Number of Iterations "+str(iterations) 
-
+	
+	#evaluating the test data...
+	print "Evaluating the Test data..."
+	handleTestData("zoo-testSimplified.csv",weightVector,"LR-results/ouput.txt")
+	
 #Execution begins here
 if __name__ == "__main__" : main()	
