3a651a800b85f33e3e3c8afc7ffb0e0568f2ab2f
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-11-22 19:17:19 -0500

    completed 2 and 3rd questions

--- reinforcement learning/2and3solution.py
+++ reinforcement learning/2and3solution.py
@@ -7,8 +7,9 @@ import random
 rowSize = 5
 colSize = 4
 wallBlocks = [(3,1), (3,3)]
-gamma = 0.5 #exploration vs exploitation.
-alphaConstant = 0.9 # learning rate.
+epsilon = 0.5 #exploration vs exploitation...
+gamma = 0.9 #discount factor..
+alphaConstant = 0.1 # learning rate...
 
 def isWallBlock(i,j):
 	if (i,j) in wallBlocks:
@@ -91,12 +92,15 @@ def printGrid(q):
 		print s3
 		print "-"*(width*3+1)*colSize
 	
-	
+isThirdQuestion = False
 def rewardFunction(i, j):  # i, j are the row and column indices respectively...
+	global isThirdQuestion
 	if (i,j)  == (1,1):
-		return -50
+		return -50 #for falling in pit
 	elif (i,j) == (0,3):
-		return 10
+		return 10 # reaching the destination..
+	if isThirdQuestion:
+		return 0 #no negative reward for 3rd question...
 	return -1
 
 def environment(i, j, a): # i, j are the row and column indices respectively...
@@ -118,11 +122,11 @@ def environment(i, j, a): # i, j are the row and column indices respectively...
 	inew, jnew = move(effect, i, j)
 	return (inew, jnew, rewardFunction(inew, jnew))	
 	
-#The main function that calls all other functions, execution begins here
-def main():	
-	random.seed()
+#The 2nd question
+def secondQuestion():	
 	global rowSize
 	global colSize
+	global epsilon
 	global gamma
 	global alphaConstant
 	acts = ["up", "down", "left", "right"]
@@ -134,7 +138,7 @@ def main():
 	while True:
 		shldEpisodeRestart = False
 		while shldEpisodeRestart==False:
-		
+				
 			if s==(0, colSize-1): #if the final state is reached, then we can end this episode.
 				shldEpisodeRestart = True
 				
@@ -146,9 +150,12 @@ def main():
 				if q[s[0]][s[1]][key] > maxValue:
 					maxValue = q[s[0]][s[1]][key]
 					maxAction = key		
-
-			val = random.choice(range(2))
-			if val == 0: # choose the action with maximum Q value
+			
+			#assume that only the 4 digits after the decimal notation are important for epsilon.
+			# epsilonRound is always less than 10000
+			epsilonRound = round(epsilon*1000)			
+			val = random.choice(range(1000))
+			if val > epsilonRound: # choose the action with maximum Q value
 				action = maxAction
 			else:
 				#explore with uniform distribution.
@@ -172,18 +179,97 @@ def main():
 			s = (inew, jnew)
 		
 		s = (rowSize-1, 0) #reset the state to start position of the episode.
-		numEpisodes+=1	
-		#s[0] = inew
-		#s[1] = jnew
-		#update iteration count..
-		
+		numEpisodes+=1	#update iteration count..
 		if numEpisodes%10 ==0:
 			#decrement the exploration on every ten episodes.
-			gamma = gamma*0.99999
-		
-		if numEpisodes>10000:
+			epsilon = epsilon/(1+epsilon)
+			
+		if numEpisodes>100000:
 			break
-				
+
+	print "*"*30
+	print "Grid for 2nd Question is "				
+	print "*"*30				
 	printGrid(q)
+	
+	
+def thirdQuestion():
+	global rowSize
+	global colSize
+	global epsilon
+	global gamma
+	global alphaConstant
+	
+	epsilon = 0.5 #exploration vs exploitation... #reinit epsilon as it is changed by 2nd question.
+	acts = ["up", "down", "left", "right"]
+	q = [[{"right":0, "left":0, "up":0, "down":0} for a in range(colSize)] for b in range(rowSize)]  #init the all q(s,a) to 0..
+	
+	s = (rowSize-1, 0) #start at the bottom left 
+	numEpisodes=0
+	
+	while True:
+		shldEpisodeRestart = False
+		while shldEpisodeRestart==False:
+				
+			if s==(0, colSize-1): #if the final state is reached, then we can end this episode.
+				shldEpisodeRestart = True
+				
+			#find out the best action...
+			action=""
+			maxAction = ""
+			maxValue = -1000
+			for key in acts:
+				if q[s[0]][s[1]][key] > maxValue:
+					maxValue = q[s[0]][s[1]][key]
+					maxAction = key		
+			
+			#assume that only the 4 digits after the decimal notation are important for epsilon.
+			# epsilonRound is always less than 10000
+			epsilonRound = round(epsilon*1000)			
+			val = random.choice(range(1000))
+			if val > epsilonRound: # choose the action with maximum Q value
+				action = maxAction
+			else:
+				#explore with uniform distribution.
+				val = random.choice(range(3)) # pick any other action with equal chance.
+				i = 0
+				for a in acts:
+					if a == maxAction:									
+						continue
+					elif i==val: 
+						action = a
+						break
+					i+=1
+				
+			inew, jnew, rnew = environment(s[0], s[1], action)
+			maxValNext = -1000
+			for a in acts:
+				if q[inew][jnew][a] > maxValNext:
+					maxValNext = q[inew][jnew][a]
+		
+			q[s[0]][s[1]][action] = (1-alphaConstant)*(q[s[0]][s[1]][action]) + alphaConstant*(rnew + gamma*maxValNext)
+			s = (inew, jnew)
+		
+		s = (rowSize-1, 0) #reset the state to start position of the episode.
+		numEpisodes+=1	#update iteration count..
+		if numEpisodes%10 ==0:
+			#decrement the exploration on every ten episodes.
+			epsilon = epsilon/(1+epsilon)
+			
+		if numEpisodes>100000:
+			break
+	
+	print "*"*30
+	print "Grid for 3rd Question is "				
+	print "*"*30
+	printGrid(q)
+
+def main():
+	global isThirdQuestion
+	random.seed()
+	secondQuestion()
+	isThirdQuestion = True
+	thirdQuestion()
+
 #Execution begins here
 if __name__ == "__main__" : main()	
