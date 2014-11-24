1367dc5ad8051a9244d16e0f764982243af6aa1e
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-11-23 22:57:27 -0500

    can extract features and append not at appropriate places to capture negation

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import sys
+import re
 
 #this dataset has 1000 +ves and 1000-ves.
 #this dataset has verbose reviews..
@@ -16,11 +17,11 @@ def parseDataSetOne():
 	
 	for f in liPositiveFiles:
 		fil = open(dirPositive+"/"+f, "r")
-		liPosReviews.append(fil.read())
-	
+		liPosReviews.append(fil.read().lower()) #features are case insensitive.. (another draw back ?)
+		
 	for f in liNegativeFiles:
 		fil = open(dirNegative+"/"+f, "r")
-		liNegReviews.append(fil.read())	
+		liNegReviews.append(fil.read().lower())	
 	
 	return (liPosReviews, liNegReviews)
 
@@ -31,17 +32,75 @@ def parseDataSetTwo():
 	positiveFile = "datasets/set2/rt-polarity.pos"	
 	
 	fil = open(positiveFile, "r")
-	liPosReviews = fil.readlines()
+	liPosReviews = [ a.lower() for a in fil.readlines()]#features are case-insensitive	
 		
 	fil = open(negativeFile, "r")
-	liNegReviews = fil.readlines()
+	liNegReviews = [a.lower() for a in fil.readlines()] #features are case-insensitive
 	
-	print len(liPosReviews)
-	print len(liNegReviews)
 	return (liPosReviews, liNegReviews)
 
 
+
+#ignores punctuations in words..(it is a drawback...)
+def getFeatures(liPosReviews, liNegReviews):	
+	#our features are unigram features...
+	liFeatures = []
+	for r in liPosReviews: #get word lists from positive reviews.
+		wordList = re.sub("[^\w]", " ",  r).split() #replace all non-word characters with space, and then split on spaces..		
+		liFeatures.extend(wordList)
+	
+	for r in liNegReviews: #get word lists from negative reviews..
+		wordList = re.sub("[^\w]", " ",  r).split() #replace all non-word characters with space, and then split on spaces..		
+		liFeatures.extend(wordList)
+	
+	#do unique on features..
+	liFeatures = list(set(liFeatures))
+	return liFeatures
+
+tempReviews=[]
+
+def doPreProcessing(reviews):
+	global tempReviews
+	#we prepend "not" to add the negation information to all words followed by a "not" and before a punctuation.
+	liPuncts  = [',', ';', '!'] # can we use any other punctuation mark TODO
+		
+	for i in range(len(reviews)):
+		finalReview = ""
+		sentences = reviews[i].split('.') # this is doomed to fail, like in the cases "Mr." and "W.W.E."		
+		for s in sentences:
+			modSen = ""
+			words = s.split(" ")
+			shldPrepend = False
+			for j in range(len(words)):
+				if words[j] == "not": #can we use any other negation word ???? TODO
+					shldPrepend = True
+
+				elif (words[j] in liPuncts) and shldPrepend==True:
+					shldPrepend=False
+										
+				elif shldPrepend == True:
+					modSen += "NOT"
+					
+				modSen += words[j]					
+				modSen += " "
+					
+			finalReview += modSen
+		reviews[i] = finalReview
+	
+	return reviews
+					
 def main():
-	#parseDataSetOne()
-	parseDataSetTwo()
+	global tempReviews
+	#liPosReviews, liNegReviews = parseDataSetOne()
+	liPosReviews, liNegReviews = parseDataSetTwo() #parse the data set..
+	
+	liPosReviews = doPreProcessing(liPosReviews)
+	liNegReviews = doPreProcessing(liNegReviews)
+	
+	liFeatures = getFeatures(liPosReviews, liNegReviews) 
+	print liFeatures
+	
+	
+	
+	
 if __name__ == "__main__" : main()	
